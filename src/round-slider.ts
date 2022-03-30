e216dd3a25c012fef5470f7d9c95144f9ff0b92e
leineveber <egor.leineveber@altence.com>
2022-03-30 23:25:09 +0300

    fix(slider): return to state without border

--- src/round-slider.ts
+++ src/round-slider.ts
@@ -34,7 +34,6 @@ export class RoundSlider extends LitElement {
   private _reverseOrder?: boolean;
   private _rotation?: {
     handle: HTMLElement;
-    handleBorder: Element;
     min: number;
     max: number;
     start: string;
@@ -201,21 +200,15 @@ export class RoundSlider extends LitElement {
       handle = handle.nextElementSibling as HTMLElement;
 
     if (!handle.classList.contains("handle")) return;
-    const handleBorder = this.shadowRoot.querySelector(`#${handle.id}-border`);
     handle.setAttribute(
       "stroke-width",
       String(2 * this.handleSize * this.handleZoom * this._scale)
-    );
-    handleBorder.setAttribute(
-      "stroke-width",
-      String(2 * (this.handleSize + 1) * this.handleZoom * this._scale)
     );
 
     const min = handle.id === "high" ? this.low : this.min;
     const max = handle.id === "low" ? this.high : this.max;
     this._rotation = {
       handle,
-      handleBorder,
       min,
       max,
       start: this[handle.id],
@@ -227,14 +220,9 @@ export class RoundSlider extends LitElement {
 
   private _cleanupRotation(): void {
     const handle = this._rotation.handle;
-    const handleBorder = this._rotation.handleBorder;
     handle.setAttribute(
       "stroke-width",
       String(2 * this.handleSize * this._scale)
-    );
-    handleBorder.setAttribute(
-      "stroke-width",
-      String(2 * (this.handleSize + 1) * this._scale)
     );
 
     this._rotation = undefined;
@@ -411,17 +399,6 @@ export class RoundSlider extends LitElement {
           stroke="rgba(0,0,0,0)"
           stroke-width="${4 * this.handleSize * this._scale}"
           />
-          <path
-          id=${`${id}-border`}
-          class="handle-border"
-          d="
-          M ${pos.x} ${pos.y}
-          L ${pos.x + 0.001} ${pos.y + 0.001}
-          "
-          vector-effect="non-scaling-stroke"
-          stroke-width="${2 * (this.handleSize + 1) * this._scale}"
-          tabindex="0"
-          />
         <path
           id=${id}
           class="handle"
@@ -539,9 +516,6 @@ export class RoundSlider extends LitElement {
       }
       g.high.handle {
         stroke: var(--round-slider-high-handle-color);
-      }
-      .handle-border {
-        stroke: var(--round-slider-handle-border-color);
       }
       svg[disabled] g.handles {
         stroke: var(--round-slider-disabled-bar-color, darkgray);
